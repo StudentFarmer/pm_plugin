0edd0aec23e4a6b8eb485d3dd73467dff5dbff59
StudentFarmer <33483576+StudentFarmer@users.noreply.github.com>
2018-10-02 14:54:22 +0900

    Create item.php

---
+++ WATER_BREATHING/src/water/item.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace water;
+use pocketmine\item\Item;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\event\player\PlayerItemHeldEvent;
+
+class item extends PluginBase implements Listener{
+ 
+ public function onEnable() {
+            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        }
+        
+public function Main($i){
+if(($i->getItem()->getID()===351:6)){
+ $ef = new EffectInstance(Effect::getEffect(Effect::WATER_BREATHING), self::MAX_DURATION, 0);
+				
+                $player->addEffect($ef);
+            } else {
+                $player->removeEffect(Effect::WATER_BREATHINGSSS);
+            }
+            }
